3b0654a142ef73d42b7fb98740068cacd79d0e4a
JustAnotherHomoSapien <t.mouinet@gmail.com>
2022-07-27 15:31:06 +0100

    Implemented Bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -18,6 +19,7 @@ class AlienInvasion:
         """Set the background color."""
         self.bg_color = (self.settings.bg_color)
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
     
     def _check_events(self):
         """Watch for keyboard and mouse events."""
@@ -36,6 +38,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -43,11 +47,27 @@ class AlienInvasion:
         if event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        if len(self.bullets) < self.settings.bullets_allowed:      
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        #Update bullet positions.
+        self.bullets.update()
+        #Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
     
     def _update_screen(self):
         """Update images on the screen and flip to the new screen."""
         self.screen.fill(self.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()
 
     def run_game(self):
@@ -55,8 +75,10 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
-            
+
+
 
 if __name__ == '__main__':
     #Make a game instance and run the game.
